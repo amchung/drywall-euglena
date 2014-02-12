6e2c103a114f1ab4182ae3bd763f2085c66d23c5
Alice-MacAir <amchung@stanford.edu>
2014-02-12 14:47:51 -0800

    combine d3 svg

--- public/views/account/spectate/object.js
+++ public/views/account/spectate/object.js
@@ -30,7 +30,7 @@ function setupD3() {
         .attr("height", 150)
         .attr("viewBox", "40 40 220 220")
         .style("opacity", "0.5")
-        .attr("transform", "translate(300)");
+        .attr("transform", "translate(30)");
         
     var shape_bg = svg_led.append("svg:rect")
     							.attr("width", 300)
