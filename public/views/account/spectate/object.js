e6e274b6a912a3b7c09d43c6cb1b639d65783e95
Alice-MacAir <amchung@stanford.edu>
2014-02-12 14:51:52 -0800

    combine d3 svg

--- public/views/account/spectate/object.js
+++ public/views/account/spectate/object.js
@@ -28,9 +28,9 @@ function setupD3() {
     svg_led = d3.select("#canvasArea").append("svg:svg")
         .attr("width", 150)
         .attr("height", 150)
-        .attr("viewBox", "40+30 40 220 220")
+        .attr("viewBox", "70 40 220 220")
         .style("opacity", "0.5")
-        .attr("transform", "translate(0, 30)");
+        .attr("transform", "translate(30, 0)");
         
     var shape_bg = svg_led.append("svg:rect")
     							.attr("width", 300)
