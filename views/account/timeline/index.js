e79ed43126a552532f77adf28fff2482a053a0d4
Alice-MacAir <amchung@stanford.edu>
2014-02-25 13:27:06 -0800

    added model

--- views/account/timeline/index.js
+++ views/account/timeline/index.js
@@ -1,5 +1,31 @@
-'use strict';
+/*'use strict';
 
 exports.init = function(req, res){
   res.render('account/timeline/index');
+};
+*/
+
+'use strict';
+
+var getReturnUrl = function(req) {
+  var returnUrl = req.user.defaultReturnUrl();
+  if (req.session.returnUrl) {
+    returnUrl = req.session.returnUrl;
+    delete req.session.returnUrl;
+  }
+  return returnUrl;
+};
+
+exports.init = function(req, res){
+  if (req.isAuthenticated()) {
+    res.redirect(getReturnUrl(req));
+  }
+  else {
+    res.render('account/timeline/index', {
+      oauthMessage: '',
+      oauthTwitter: !!req.app.get('twitter-oauth-key'),
+      oauthGitHub: !!req.app.get('github-oauth-key'),
+      oauthFacebook: !!req.app.get('facebook-oauth-key')
+    });
+  }
 };
