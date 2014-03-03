56b480884158ffd2c857b880d25e43dcb0af3716
Alice-MacAir <amchung@stanford.edu>
2014-03-03 13:46:43 -0800

    animation test

--- public/views/account/timeline/index.js
+++ public/views/account/timeline/index.js
@@ -112,8 +112,8 @@ var hour = d3.time.format("%I"),
   
   function mouseclick(d){
   	console.log(d.time);
-  	d3.select(this).transition().duration(300)
-        .style("background-color", "#333");
+  	d3.select(this).transition().duration(1000)
+        .style("width", cellWidth*2);
   }
   
   socket = io.connect('http://171.65.102.132:3006');
