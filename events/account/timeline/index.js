da19538321b06032bdaa609831c1d1cd853f3e87
Alice-MacAir <amchung@stanford.edu>
2014-03-08 11:09:30 -0800

    redis migration

--- events/account/timeline/index.js
+++ events/account/timeline/index.js
@@ -1,5 +1,5 @@
 'use strict';
-var io = require('socket.io-client');
+/*var io = require('socket.io-client');
 var ex_socket = new io.connect("http://171.65.102.132:3006");
 var myClock;
 var currenttime;
@@ -16,7 +16,7 @@ ex_socket.on('disconnect', function() {
 
 ex_socket.on('postblocks', function(data){
   	socket.emit('/timeline/#postBlocks', data);
-});
+});*/
 
 exports.join = function(app, socket){
 	console.log("////////////join//////////////");
@@ -32,13 +32,89 @@ exports.join = function(app, socket){
 
 exports.callblocks = function(app, socket){
   return function(message) {
-  	var beginT = message.begintime;
+	var redis = require("redis"),
+	 client = redis.createClient();
+	 
+	// get max limit
+	var timeline_end;
+	client.get('global:next_tb_id', function(err,res){
+		if (err){
+			console.log("error: "+err);
+		}
+			timeline_end =res;
+			getblockIDs();
+	});
+	
+	// convert dates and get block ids
+	var beginT = message.begintime;
 	var endT = message.endtime;
 	console.log(beginT);
 	console.log(endT);
-	socket.visitor = socket.handshake.user;
+	
+	var begintime = beginT.getTime();
+	var endtime = endT.getTime();
+	
+	var firstid;
+	var lastid;
+	var commands = [];
+	
+	function getblockIDs(){
+		client.get("tb_time:"+begintime+":tb_id", function(err,res){
+			if (err){
+				console.log("error: "+err);
+			}
+			firstid = res;
+			// if out of range
+			if (firstid == null){
+				firstid = 0;
+				lastid = firstid + (3*60/5);
+			}
+			console.log("looking up : block # "+firstid);
+		
+			client.get("tb_time:"+endtime+":tb_id", function(err,res){
+				if (err){
+					console.log("error: "+err);
+				}
+				lastid = res;
+				// if out of range
+				if (lastid == null)
+				{
+					lastid = timeline_end;
+					firstid = lastid - (3*60/5);
+				}
+				console.log(" ~  block # " +lastid);
+			
+				runCommands(firstid, lastid);
+			});
+		});
+	}
+	
+	function runCommands(firstid, lastid){
+		var first = parseInt(firstid);
+		var last = parseInt(lastid);
+		console.log(first+"~"+last);
+		
+		for (var i=first;i<=last;i++){
+			commands.push(["get","tb_id:"+i+":time"]);
+			commands.push(["get","tb_id:"+i+":locked"]);
+			commands.push(["get","tb_id:"+i+":user_id"]);
+			commands.push(["get","tb_id:"+i+":exp_id"]);
+			commands.push(["get","tb_id:"+i+":pattern_id"]);
+			commands.push(["get","tb_id:"+i+":past"]);
+			commands.push(["get","tb_id:"+i+":admin"]);
+			commands.push(["get","tb_id:"+i+":current"]);
+			commands.push(["get","tb_id:"+i+":image"]);
+		}
+		client.multi(commands).exec(function (err, res) {
+			if(err){
+				console.log("error: "+err);
+			}else{
+				socket.emit('postblocks',  _.toArray(res) );
+			}
+		});
+	}
+
 	// need to add user log on the timeline server-side
-	ex_socket.emit('timeline', { type: 'callblocks', userid: socket.visitor.id, begintime: beginT, endtime: endT});
   };
 };
 
