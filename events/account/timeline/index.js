e165d2280bb6940ec3971e441a5190e7e08096d2
Alice-MacAir <amchung@stanford.edu>
2014-03-08 00:02:23 -0800

    socket io

--- events/account/timeline/index.js
+++ events/account/timeline/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 exports.join = function(app, socket){
+	console.log("////////////join//////////////");
   return function() {
     socket.visitor = 'guest';
     if (socket.handshake.user) {
