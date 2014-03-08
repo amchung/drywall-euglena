5e07df99107cbed256a717b72baaf1fa1fc3c14a
Alice-MacAir <amchung@stanford.edu>
2014-03-07 23:57:14 -0800

    socket io

--- events/account/timeline/index.js
+++ events/account/timeline/index.js
@@ -9,19 +9,19 @@ exports.join = function(app, socket){
     }
 
     socket.join('/account/timeline/');
-    socket.broadcast.to('/account/timeline').emit('/account/timeline/#newUser', socket.visitor);
+    socket.broadcast.to('/account/timeline').emit('/timeline/#newUser', socket.visitor);
   };
 };
 
 exports.callblocks = function(app, socket){
   return function(message) {
-    socket.broadcast.to('/account/timeline').emit('/about/timeline/#postBlock', socket.visitor, message);
+    socket.broadcast.to('/account/timeline').emit('/timeline/#postBlock', socket.visitor, message);
   };
 };
 
 /*exports.reserve = function(app, socket){
   return function(message) {
-    socket.broadcast.to('/account/timeline').emit('/about/timeline/#incoming', socket.visitor, message);
+    socket.broadcast.to('/account/timeline').emit('/timeline/#incoming', socket.visitor, message);
   };
 };
 */
