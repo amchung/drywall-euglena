147a704091c47953c5c9b26c1824d6bb41e4247b
Alice-MacAir <amchung@stanford.edu>
2014-03-17 14:29:28 -0700

    access

--- events/account/lab/index.js
+++ events/account/lab/index.js
@@ -5,20 +5,28 @@ exports.labaccess = function(app, socket){
    	console.log("////////////enter lab//////////////");
    	socket.visitor = 'guest';
     if (socket.handshake.user) {
-      //socket.visitor = socket.handshake.user.username;
       socket.visitor = socket.handshake.user.id;
       socket.username = socket.handshake.user.username;
-    	console.log(socket.username);
+    	//console.log(socket.username);
     }
     var redis = require("redis"),
 	 client = redis.createClient();
 	var _ = require('underscore');
-    
     client.get("global:current:tb_id", function(err,res){
+    	var current_block;
 		if (err){
 			console.log("error: "+err);
 		}else{
-		console.log("current block:" +res);
+			current_block = res;
+			console.log("current block:" + current_block);
+			
+			client.get("tb_id:"+current_block+":username", function(err,res){
+				if (err){
+					console.log("error: "+err);
+				}else{
+					console.log("current block owner:" + res);
+				}
+			});
 		}
 	});
   };
