8101766bb56e2b4537e534a986336563669e40cb
Alice-MacAir <amchung@stanford.edu>
2014-04-07 13:03:57 -0700

    svg to load jpg

--- public/views/account/lab/lab.js
+++ public/views/account/lab/lab.js
@@ -431,14 +431,15 @@ function getVideo(){
 		vid_context.clearRect(0, 0, vid_width, vid_height);
 		vid_context.drawImage(image, 0, 0, vid_width, vid_height);
 	});*/
-	//var old = document.getElementById("img_snapshot");
+	var old = document.getElementById("img_snapshot");
 	//old.parentNode.removeChild(old);
+	old.attr('xlink:href',"http://171.65.102.132:8080/?action=snapshot?t=" + new Date().getTime());
 	
-	vid_canvas.append("image")
+	/*vid_canvas.append("image")
 		.attr("width", vid_width)
 		.attr("height", vid_height)
 		.attr("id", "img_snapshot")
-		.attr('xlink:href',"http://171.65.102.132:8080/?action=snapshot?t=" + new Date().getTime());
+		.attr('xlink:href',"http://171.65.102.132:8080/?action=snapshot?t=" + new Date().getTime());*/
 }
 
 function getVidFrame(path, callback) {
