543e6427f30e56da460f676c8879facc2e686ec5
Alice-MacAir <amchung@stanford.edu>
2014-04-15 11:15:58 -0700

    data array

--- public/views/account/replay/index.js
+++ public/views/account/replay/index.js
@@ -23,6 +23,7 @@ var imageTime;
   
   socket.on('/replay/#postdata', function(data){
   	blockData = [];
+	ledTime = [];
 	blockData.push(data[0]); // time
 	blockData.push(data[1]); // owner
 	blockData.push(data[2]); // experiment id
