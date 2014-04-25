2bff31b72e906d72a6f5d04ddf7f9f511e1c770f
Alice-MacAir <amchung@stanford.edu>
2014-04-25 14:33:37 -0700

    pattern

--- events/account/pattern/index.js
+++ events/account/pattern/index.js
@@ -22,29 +22,45 @@ exports.setpattern = function(app, socket){
   	// convert dates and get block ids
     console.log("Block "+ message.targetBlock + " : set pattern");
     var target_id = message.targetBlock;
+    var target_exp_id;
     var target_pattern_id;
-			
-    client.get("global:next_pattern_id", function(err,res){
-	    if (err){
-		    console.log("error: "+err);
-	    }else{
-		    target_pattern_id = res;
-		    console.log("current next_pattern_id: "+target_pattern_id);
-		    client.set("tb_id:"+target_id+":pattern_id",target_pattern_id, function(err){
-			    if (err){
-				    console.log("error: "+err);
-			    }else{
-				    client.incr("global:next_exp_id");
-				    client.set("pattern_id:"+target_pattern_id, message.pattern, function(err){
+    
+    client.get("global:next_exp_id", function(err,res){
+	if (err){
+		console.log("error: "+err);
+	}else{
+		target_exp_id = res;
+		console.log("current next_exp_id: "+target_exp_id);
+		client.set("tb_id:"+target_id+":exp_id",target_exp_id, function(err){
+			if (err){
+				console.log("error: "+err);
+			}else{
+				client.incr("global:next_exp_id");			
+				client.get("global:next_pattern_id", function(err,res){
 					if (err){
 						console.log("error: "+err);
 					}else{
-						socket.emit('/pattern/#patternsaved',"pattern saved as pattern # "+target_pattern_id + " for block # " + target_id);
+						target_pattern_id = res;
+						console.log("current next_pattern_id: "+target_pattern_id);
+						client.set("tb_id:"+target_id+":pattern_id",target_pattern_id, function(err){
+							if (err){
+								console.log("error: "+err);
+							}else{
+								client.incr("global:next_pattern_id");
+								client.set("pattern_id:"+target_pattern_id, message.pattern, function(err){
+								    if (err){
+									    console.log("error: "+err);
+								    }else{
+									    socket.emit('/pattern/#patternsaved',"pattern saved as pattern # "+target_pattern_id + " for block # " + target_id);
+								    }
+								});
+							}
+						});
 					}
-				    });
-			    }
-		    });
-	    }
+				});
+			}
+		});
+	}
     });
-  };
+  }
 };
