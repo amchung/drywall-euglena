bd08c822c6c6c6e61afd81aacd777424da65683c
Alice-MacAir <amchung@stanford.edu>
2014-06-03 11:48:37 -0700

    datepicker

--- public/views/account/timeline/index.js
+++ public/views/account/timeline/index.js
@@ -546,6 +546,19 @@ var hour = d3.time.format("%I"),
     document.getElementById("btn_close").style.display="none";
   });
   
-  $('#datepicker').datepicker();
-    
+  var nowTemp = new Date();
+  var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+ 
+  var checkin = $('#datepicker').datepicker({
+    onRender: function(date) {
+      return date.valueOf() < now.valueOf() ? 'disabled' : '';
+    }
+  }).on('changeDate', function(ev) {
+    if (ev.date.valueOf() > checkout.date.valueOf()) {
+      var newDate = new Date(ev.date)
+      newDate.setDate(newDate.getDate() + 1);
+      checkout.setValue(newDate);
+    }
+  }).data('datepicker');
+  
 }());
