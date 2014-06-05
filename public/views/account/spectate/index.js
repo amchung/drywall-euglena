41cd94ee30fca1338906b71970a710e8e5b2eee2
Alice-MacAir <amchung@stanford.edu>
2014-06-05 11:52:10 -0700

    date time picker css

--- public/views/account/spectate/index.js
+++ public/views/account/spectate/index.js
@@ -2,10 +2,43 @@
 
 var socket;
 var myname;
-
+var clock_socket;
 
 (function() {
   'use strict';
+  
+  var clockbar = $('#clock_bar');
+  var myClock;
+  function myTimer(){
+		// >>>>>> socket: look clock
+		clock_socket.emit('lookclock');
+  }
+  clock_socket = io.connect('http://171.65.102.132:3006');
+  
+  clock_socket.on('server_clock', function(data){
+  	var str = data.split(":");
+  	if(str[1]=='00'){
+  	  callBlocks(currenttime);
+  	}
+	if ((str[0]=="0")&&(str[1]=='10')) {
+	  callBlocks(currenttime);
+	}
+  	if((str[0]=="0")&&(parseInt(str[1]))<10){
+  		clockbar.html("<b><font color='red'>"+data+"</font><b>");
+  	}
+  	else{
+  		clockbar.html("<b>"+data+"</b>");
+  	}
+  });
+
+  clock_socket.on('connect', function() {
+	console.log("Clock connected!");
+	myClock=setInterval(function(){myTimer()},500);
+  });
+
+  clock_socket.on('disconnect', function() {
+	console.log('Clock lost');
+  });
 
   socket = io.connect();
   socket.on('connect', function(){
