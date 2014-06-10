234aa6ef07cfe3a08941749639d0aa7acd4cb87c
Alice-MacAir <amchung@stanford.edu>
2014-06-09 17:49:52 -0700

    new schema

--- events/account/pattern/index.js
+++ events/account/pattern/index.js
@@ -123,6 +123,7 @@ exports.listblocks = function(app,socket){
     var _ = require("underscore");
     
     client.zrange("user_id:"+socket.username+":tb_id",0,-1, function(err,res){
+	console.log(res);
 	socket.emit('/pattern/#postblocks',  res );
     });
   }
