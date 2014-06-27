1fdf96095e462c09522c921064795039876931aa
Alice-MacAir <amchung@stanford.edu>
2014-06-27 11:42:39 -0400

    pattern editor debug

--- public/views/account/pattern/index.js
+++ public/views/account/pattern/index.js
@@ -212,7 +212,7 @@ function setupVis(){
 }
 
 function drawVis(data){
-    svg_vis.selectAll('.plot').enter().selectAll().remove();
+    svg_vis.selectAll('.plot').remove();
     
     var pattern = svg_vis.selectAll('.plot')
 	  .data(data)
